2e4390abf7650679f7dba15e0fedd0935dc0dc83
avinwijaya <98245922+avinwijaya@users.noreply.github.com>
2025-05-14 15:06:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,6 @@ def get_bmkg_weather(kode_lokasi):
         return jsonify({"error": "Tag <script id='__NUXT_DATA__'> tidak ditemukan"}), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
